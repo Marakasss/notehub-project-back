ba85ec383ca0cc14a9e13229e891ff579d309a2b
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-11-14 19:12:15 +0200

    get user route added

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,11 +3,16 @@ import { authenticate } from '../middlewares/aunthenticate.js';
 import { upload } from '../middlewares/multer.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { updateUserSchema } from '../validation/user.js';
-import { patchUserInfoController } from '../controllers/users.js';
+import {
+  getCurrentUserController,
+  patchUserInfoController,
+} from '../controllers/users.js';
 
 const router = Router();
 
 router.use(authenticate);
+
+router.get('/me', getCurrentUserController);
 
 router.patch(
   '/me',
